0584f2e793150a82c2caf47e7fd88e90180f7abf
Lucas Vellido <lucas@ampliato.com.br>
2020-07-03 11:12:06 -0300

    change coloar buttom

--- lib/main.dart
+++ lib/main.dart
@@ -75,7 +75,7 @@ class _HomeState extends State<Home> {
           ),
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
+        selectedItemColor: Colors.amber[700],
         onTap: _onItemTapped,
       ),
     );
